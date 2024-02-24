6911953d7d6957f905b91ae5c3801b630e088e4c
sultanovttIOS <alisher192377@gmail.com>
2024-02-24 18:01:34 +0600

    Initial Commit

---
+++ HW5.1MyNotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW5.1MyNotes
+//
+//  Created by Alisher Sultanov on 24/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
