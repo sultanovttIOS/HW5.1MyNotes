4669513ece1c425ac29634edcdc83f3044ebbd7d
sultanovttIOS <alisher192377@gmail.com>
2024-03-01 20:27:12 +0600

    add MVC and complete task

---
+++ HW5.1MyNotes/Modules/Settings/SettingsController.swift
@@ -0,0 +1,8 @@
+//
+//  SettingsController.swift
+//  HW5.1MyNotes
+//
+//  Created by Alisher Sultanov on 1/3/24.
+//
+
+import Foundation
---
+++ HW5.1MyNotes/Modules/Settings/SettingsModel.swift
@@ -0,0 +1,8 @@
+//
+//  SettingsModel.swift
+//  HW5.1MyNotes
+//
+//  Created by Alisher Sultanov on 1/3/24.
+//
+
+import Foundation
