1565f0a94375fe079d1b7d50b3970f65bb096f77
sultanovttIOS <alisher192377@gmail.com>
2024-03-06 19:42:07 +0600

    completed task 3

---
+++ HW5.1MyNotes/Modules/OnBoardingView/OnBoardingCell.swift
@@ -0,0 +1,12 @@
+//
+//  CollectionViewCell.swift
+//  HW5.1MyNotes
+//
+//  Created by Alisher Sultanov on 5/3/24.
+//
+
+import UIKit
+
+class OnBoardingCell: UICollectionViewCell {
+    
+}
---
+++ HW5.1MyNotes/Modules/OnBoardingView/OnBoardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnBoardingViewViewController.swift
+//  HW5.1MyNotes
+//
+//  Created by Alisher Sultanov on 5/3/24.
+//
+
+import UIKit
+
+class OnBoardingView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
